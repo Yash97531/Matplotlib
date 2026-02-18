ed209987e884dbe338f030f8944c6a7594e66839
Yash97531 <yash1980000@gmail.com>
2026-02-18 17:15:10 +0530

    Data of complany sales

---
+++ matplotlibs.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv('company_sales_data.csv')
+profit = df ['total_profit'].tolist()
+month = df['month_number'].tolist()
+plt.plot(month, profit, color = 'cyan', label = 'Total sales of data last year')
+plt.title('company profit per month')
+plt.xlabel('Month number')
+plt.ylabel('Total Profit')
+plt.xticks(month)
+plt.yticks([100000, 200000, 300000, 400000, 500000])
+plt.show()
