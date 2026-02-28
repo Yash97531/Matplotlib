dd27fa7002a6698541ca58b63495abb5a7f031d6
Yash97531 <yash1980000@gmail.com>
2026-02-28 16:25:19 +0530

    Output.csv Question 1

--- New_Ques01.py
+++ New_Ques01.py
@@ -1,2 +1,7 @@
-
+# 1️⃣ Data Cleaning + Visualization
+# Load the dataset.
+# Find total missing values in each column.
+# Replace missing values in numeric columns using column mean (NumPy).
+# Remove rows where Feature4 is missing.
+# Plot a histogram of Feature1.
 
