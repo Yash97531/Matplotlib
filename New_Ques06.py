05ae31830a35801c4544fab56f17af57a07e3527
Yash97531 <yash1980000@gmail.com>
2026-03-10 15:53:26 +0530

    Output_csv

--- New_Ques06.py
+++ New_Ques06.py
@@ -8,8 +8,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('Matplotlib/output.csv')
 Feature08 = df['Feature8'].dropna()
-a = Feature08.sort_values(ascending=False)
-top15 = a.head(15)
-numpy_array = np.array(top15)
-plt.plot()
-print(top15)
+a = Feature08.sort_values(ascending=False).head(15)
+numpy_array = np.array(a)
+plt.plot(numpy_array, marker = 'o', )
+plt.title("Top 15 Values of Feature8")
+plt.xlabel("Index")
+plt.ylabel("Feature8 Value")
+plt.grid(True)
+
+plt.show()
