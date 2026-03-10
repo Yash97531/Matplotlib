b41d1fc0990582592fb077a650f9564efc4c9aaf
Yash97531 <yash1980000@gmail.com>
2026-03-10 15:54:47 +0530

    Output_csv

--- New_Ques06.py
+++ New_Ques06.py
@@ -1,19 +1,11 @@
-# 5 Top Values + Line Chart
-# Sort dataset by Feature8 in descending order.
-# Select top 15 rows.
-# Convert Feature8 to NumPy array.
-# Plot line chart of top 15 values.
+# 7️⃣ New Column Creation + Visualization
+# Create new column:
+# "High" if Feature1 > mean
+# "Low" otherwise
+# Count High and Low values
+# Plot bar chart.
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv('Matplotlib/output.csv')
-Feature08 = df['Feature8'].dropna()
-a = Feature08.sort_values(ascending=False).head(15)
-numpy_array = np.array(a)
-plt.plot(numpy_array, marker = 'o', )
-plt.title("Top 15 Values of Feature8")
-plt.xlabel("Index")
-plt.ylabel("Feature8 Value")
-plt.grid(True)
+df = pd.read_csv('Matpotlib/output.csv')
 
-plt.show()
